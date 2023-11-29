90d106b9e046dae9746e2d5eaa569a72b5d9ce6f
filzasaleem <filza.saleem05@gmail.com>
2023-11-29 13:34:56 +0100

    added fetch api in reducer

--- src/pages/StartPage.jsx
+++ src/pages/StartPage.jsx
@@ -1,10 +1,11 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useSelector } from "react-redux";
 
 
 export const StartPage = () => {
+  
+
   const postList = useSelector((state) => state.posts.postsList);
   console.log(postList);
   return <div>Start Page</div>;
-
 };
