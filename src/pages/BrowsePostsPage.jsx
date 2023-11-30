0e2d9a909aa57d652defe8f7f637fb462438f21e
Vardagsbruket <134447488+Vardagsbruket@users.noreply.github.com>
2023-11-30 10:55:53 +0100

    add ListPosts on StartPage and BrowsePost + add navigation functionality

--- src/pages/BrowsePostsPage.jsx
+++ src/pages/BrowsePostsPage.jsx
@@ -1,5 +1,15 @@
-import React from "react";
+import { ListPosts } from "../components/ListPosts";
 
 export const BrowsePostsPage = () => {
-  return <div>B</div>;
+  return (
+    <>
+      <div>
+        <h2>Browse Posts</h2>
+      </div>
+      <div>
+        <h3>Filter component</h3>
+      </div>
+      <ListPosts />
+    </>
+  );
 };
--- src/pages/DetailPage.jsx
+++ src/pages/DetailPage.jsx
@@ -1,5 +1,36 @@
 import React from "react";
+import { DetailPost } from "../components/DetailPost";
+import { NavLink, useParams } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 export const DetailPage = () => {
-  return <div>D</div>;
+  const { postId } = useParams();
+  const postList = useSelector((state) => state.posts.postsList);
+  const post = postList.find((post) => post.id === parseInt(postId));
+  console.log("post id:", postId);
+  console.log("post found", post);
+  return (
+    <>
+      <div>
+        <NavLink to={-1}>
+          <button>Go Back</button>
+        </NavLink>
+        <div>
+          <img src="" alt="" />
+          <div>
+            <p>{post.type}</p>
+            <h2>{post.postTitle}</h2>
+            <p>{post.category}</p>
+            <div>
+              <p>{post.description}</p>
+              <p>{post.city}</p>
+              <p>{post.contactInfo}</p>
+              <p>{post.createdDate}</p>
+            </div>
+          </div>
+        </div>
+        <p></p>
+      </div>
+    </>
+  );
 };
