7ea4aac855ca5a7aa5214e52e01862b4d4aa742a
Vardagsbruket <134447488+Vardagsbruket@users.noreply.github.com>
2023-12-03 20:48:09 +0100

    add AboutPage with content + AboutPage.css + route

---
+++ src/pages/AboutPage.jsx
@@ -0,0 +1,24 @@
+import "./AboutPage.css";
+export const AboutPage = () => {
+  return (
+    <div className="about">
+      <p>
+        This is app is created as our final project for Technigo's upskill
+        course i JavaScript and React. The mission was to create a project that
+        would focus on a societal challenge in accordance with Agenda 2030
+      </p>
+      <p>
+        The purpose of this app is to help mitigate over consumption by
+        providing a platform where neighbours can offer to lend each other
+        things that you seldom use, but sill need occasionally. Examples are
+        drilling machines, trailers for your car and skies. You can both share a
+        post with things you have available or post that you are in need of
+        borrowing something. We also wanted to provide the opportunity to give
+        away things you no longer need, but that still has good usage value.
+      </p>
+      <p>
+        The app is built using React Vite, Redux, React routes and JSON-Server.
+      </p>
+    </div>
+  );
+};
