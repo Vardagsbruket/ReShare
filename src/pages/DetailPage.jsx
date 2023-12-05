e30b5ac9d1f57bd4e46a1861c685b6b31e7a9833
Vardagsbruket <134447488+Vardagsbruket@users.noreply.github.com>
2023-12-05 21:16:10 +0100

    add styling to DetailPage

--- src/pages/DetailPage.jsx
+++ src/pages/DetailPage.jsx
@@ -1,6 +1,7 @@
 import { NavLink, useParams } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { FacebookShareButton, FacebookIcon } from "react-share";
+import "./DetailPage.css";
 import { getPostsList } from "../reducers/postSlice";
 import { useEffect, useState } from "react";
 import { Loading } from "../components/Loading";
@@ -19,7 +20,7 @@ export const DetailPage = () => {
   const postList = useSelector((state) => state.posts.postsList);
 
   useEffect(() => {
-    console.log("isNewPostCreated:", isNewPostCreated); // Add this log to see the value
+    //console.log("isNewPostCreated:", isNewPostCreated); // Add this log to see the value
     if (isNewPostCreated) {
       console.log("Banner should be displayed!");
     }
@@ -39,30 +40,39 @@ export const DetailPage = () => {
   return (
     <>
       <div className="detail-post-container">
-        <NavLink to={-1}>
-          <button>Go Back</button>
+        <NavLink to={-1} className="go-back">
+          Go Back
         </NavLink>
         {isNewPostCreated && <CreatePostBanner />}
         {isFetched ? (
           post && post._id ? (
-            <div>
-              <img src="" alt="" />
+            <div className="detail-flex-container">
+              <img
+                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQXUDvACls89tR8w9E3DL8FQV_-lK42f3-Gww&usqp=CAU"
+                alt=""
+                className="detail-image"
+              />
+
               <div className="detail-post-wrapper">
-                <p>{post.type}</p>
-                <h2>{post.postTitle}</h2>
-                <p>{post.category}</p>
-                <div>
-                  <p>{post.description}</p>
-                  <p>{post.city}</p>
-                  <p>{post.contactInfo}</p>
-                  <p>{post.createdDate}</p>
-                </div>
-                <div>
-                  <FacebookShareButton
-                    url={`https://stirring-florentine-c4bb3f.netlify.app/post/${post._id}`}
-                  >
-                    <FacebookIcon size={32} round />
-                  </FacebookShareButton>
+                <p className="type">{post.type}</p>
+                <h2 className="title">{post.postTitle}</h2>
+                <p className="medium-text">
+                  {post.category} | {post.city}
+                </p>
+                <div className="detail-text-container">
+                  <p className="small-text">{post.description}</p>
+                  <p className="medium-text">Contact</p>
+                  <p className="small-text">{post.contactInfo}</p>
+                  <p className="medium-text">Creation date</p>
+                  <p className="small-text">{post.createdDate}</p>
+                  <div>
+                    <FacebookShareButton
+                      url={`https://stirring-florentine-c4bb3f.netlify.app/post/${post._id}`}
+                      className="facebook"
+                    >
+                      <FacebookIcon size={32} round />
+                    </FacebookShareButton>
+                  </div>
                 </div>
               </div>
             </div>
