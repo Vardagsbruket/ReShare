4d7fa80f187f71bdaae36a8ca02af51329010f41
Vardagsbruket <134447488+Vardagsbruket@users.noreply.github.com>
2023-12-05 14:59:40 +0100

    fix bug in post id when redirecting too DetailPage on form submit + add global state for new post" (#23)
    * add css styling to CardPost
    
    * add css styling to ListPosts, header, filter and footer
    
    * add css styling to ListPosts, header, filter and footer
    
    * fix typo in CreatePost _id
    
    * fix bug in redirecting to DetailPage on form submit + add global state when CreatePost

--- src/components/CreatePost.jsx
+++ src/components/CreatePost.jsx
@@ -1,7 +1,11 @@
 import { useState } from "react";
 import "./CreatePost.css";
 import { useDispatch, useSelector } from "react-redux";
-import { setNewPostCreated, getPostsList } from "../reducers/postSlice";
+import {
+  setNewPostCreated,
+  getPostsList,
+  createNewPost,
+} from "../reducers/postSlice";
 import { useNavigate } from "react-router-dom";
 
 export const CreatePost = () => {
@@ -13,56 +17,65 @@ export const CreatePost = () => {
     category: "",
     type: "",
     city: "",
-    createdDate:null
+    createdDate: null,
   };
-  
-  const [newPost,setNewPost] = useState(initialState);
+
+  const [newPost, setNewPost] = useState(initialState);
   const categoryList = useSelector((state) => state.posts.categoryList);
   const cityList = useSelector((state) => state.posts.cityList);
 
-  const createNewPost = async () => {
-    try {
-      const url = "/.netlify/functions/create_post";
-      const response = await fetch(url, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(
-          newPost
-        ),
-      });
+  // const createNewPost = async () => {
+  //   try {
+  //     const url = "/.netlify/functions/create_post";
+  //     let createdPost = {
+  //       createdDate: new Date().getTime(),
+  //       postTitle: title,
+  //       description: description,
+  //       contactInfo: contact,
+  //       type: type,
+  //       city: city,
+  //       category: category,
+  //     };
+  //     const response = await fetch(url, {
+  //       method: "POST",
+  //       headers: {
+  //         "Content-Type": "application/json",
+  //       },
+  //       body: JSON.stringify(createdPost),
+  //     });
+  //     if (response.ok) {
+  //       const data = await response.json(); // Assuming the server returns the created post data
+  //       createdPost._id = data.insertedId;
+  //       // checking if the a post was newly created
+  //       setCreateNewPostSuccess(true);
+  //       dispatch(setNewPostCreated(true));
+  //       // Redirect to the page of the newly created post
+  //       redirect(`/post/${createdPost.insertedId}`);
+  //     }
+  //   } catch (error) {
+  //     console.error("Error creating new post:", error);
+  //   }
+  // };
 
-      if (response.ok) {
-        const createdPost = await response.json(); // Assuming the server returns the created post data
-        // checking if the a post was newly created
-        setCreateNewPostSuccess(true);
-        console.log("New post created:", createdPost);
-
-        dispatch(setNewPostCreated(true));
-        // Redirect to the page of the newly created post
-        redirect(`/post/${createdPost._id}`);
-      }
-
-      const data = await response.json();
-      console.log(data);
-      console.log("New post created");
-    } catch (error) {
-      console.error("Error creating new post:", error);
-    }
-  };
-  const handleChange = (key, value) => {
-    setNewPost({ ...newPost, [key]: value });
-  };
-  
-
-  const handleFormSubmit = async(e) => {
+  const handleFormSubmit = async (e) => {
     e.preventDefault();
-    setNewPost((prev) => ({ ...prev, createdDate: new Date().getTime() }));
-    await createNewPost();
-    setNewPost(prevState => ({ ...prevState, ...initialState }));
-    console.log("....", newPost);
-    dispatch(getPostsList());
+
+    const newPost = {
+      createdDate: new Date().getTime(),
+      postTitle: title,
+      description: description,
+      contactInfo: contact,
+      type: type,
+      city: city,
+      category: category,
+    };
+    const createdPost = await dispatch(await createNewPost(newPost));
+    setCreateNewPostSuccess(true);
+    dispatch(setNewPostCreated(true));
+    // Redirect to the page of the newly created post
+    console.log("Handle form submit:", createdPost);
+    // dispatch(getPostsList());
+    redirect(`/post/${createdPost.payload._id}`);
   };
 
   return (
@@ -71,40 +84,46 @@ export const CreatePost = () => {
       <input
         type="text"
         id="postTitle"
-        onChange={(e) => handleChange("postTitle",e.target.value)}
+        onChange={(e) => handleChange("postTitle", e.target.value)}
       ></input>
 
       <label htmlFor="description">Description:</label>
       <textarea
         id="description"
-        onChange={(e) => handleChange("description",e.target.value)}
+        onChange={(e) => handleChange("description", e.target.value)}
       ></textarea>
 
       <label htmlFor="contactInfo">Contact:</label>
       <input
         type="text"
         id="contactInfo"
-        onChange={(e) => handleChange("contactInfo",e.target.value)}
+        onChange={(e) => handleChange("contactInfo", e.target.value)}
       ></input>
 
       <label htmlFor="type">Borrow, lend or give away?</label>
-      <select id="type" onChange={(e) => handleChange("type",e.target.value)}>
-      <option value="">choose one type</option>
+      <select id="type" onChange={(e) => handleChange("type", e.target.value)}>
+        <option value="">choose one type</option>
         <option value="Needed">Needed</option>
         <option value="Available">Available</option>
         <option value="Give away">Give away</option>
       </select>
 
       <label htmlFor="category">Categories</label>
-      <select id="category" onChange={(e) => handleChange("category",e.target.value)}>
-      <option value="">choose category</option>
+      <select
+        id="category"
+        onChange={(e) => handleChange("category", e.target.value)}
+      >
+        <option value="">choose category</option>
         {categoryList.slice(1).map((category, index) => (
           <option key={index}>{category}</option>
         ))}
       </select>
       <label htmlFor="category">Select city</label>
-      <select id="category" onChange={(e) => handleChange("city",e.target.value)}>
-      <option value="">Select city</option>
+      <select
+        id="category"
+        onChange={(e) => handleChange("city", e.target.value)}
+      >
+        <option value="">Select city</option>
         {cityList.slice(1).map((city, index) => (
           <option key={index}>{city}</option>
         ))}
