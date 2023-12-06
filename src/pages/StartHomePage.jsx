1533b6cca1fb381a97537b0c97fd8ff6d8f01acb
Vardagsbruket <134447488+Vardagsbruket@users.noreply.github.com>
2023-12-06 16:47:03 +0100

    Fix conflict in StartHomePage after merge (#35)
    * fix bug in functions state.json after merge
    
    * final css fix on all pages
    
    * delete StartHomePage.css
    
    * change name on files to fix netlify deployment

--- src/pages/StartHomePage.jsx
+++ src/pages/StartHomePage.jsx
@@ -2,7 +2,6 @@ import { useSelector } from "react-redux";
 import { Hero } from "../components/Hero";
 import { StartPagePosts } from "../components/StartPagePosts";
 import { Loading } from "../components/Loading";
-import "./startHomePage.css";
 
 export const StartHomePage = () => {
   const isLoading = useSelector((state) => state.posts.isLoading);
