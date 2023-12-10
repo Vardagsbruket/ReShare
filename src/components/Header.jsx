8e77dd54ceb553789d20e617bf478d991aa03ccc
Vardagsbruket <134447488+Vardagsbruket@users.noreply.github.com>
2023-12-10 20:52:50 +0100

    add that hamburger sidebar closes automatically onClick

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,34 +5,54 @@ import Menu from "../assets/icons/menu-icon.svg";
 
 export const Header = () => {
   const [showNavigation, setShowNavigation] = useState(false);
+
   const toggleNavigation = () => {
     setShowNavigation(!showNavigation);
   };
+
+  const closeNavigation = () => {
+    if (showNavigation) {
+      setShowNavigation(false);
+    }
+  };
+
   return (
     <>
       <div className="header">
         <div>
-          <NavLink to="/" className="logo">
+          <NavLink to="/" className="logo" onClick={closeNavigation}>
             <h1>ReShare</h1>
           </NavLink>
         </div>
         <div className="menu-icon" onClick={toggleNavigation}>
-          <img src={Menu} />
+          <img src={Menu} alt="Menu" />
         </div>
         <div className={`navBar ${showNavigation && "sidebar"}`}>
           <ul>
             <li>
-              <NavLink to="/about" className="about-link">
+              <NavLink
+                to="/about"
+                className="about-link"
+                onClick={closeNavigation}
+              >
                 About
               </NavLink>
             </li>
             <li>
-              <NavLink to="/browse-posts" className="browse">
+              <NavLink
+                to="/browse-posts"
+                className="browse"
+                onClick={closeNavigation}
+              >
                 Browse Posts
               </NavLink>
             </li>
             <li>
-              <NavLink to="/create-post" className="create">
+              <NavLink
+                to="/create-post"
+                className="create"
+                onClick={closeNavigation}
+              >
                 Create Post
               </NavLink>
             </li>
